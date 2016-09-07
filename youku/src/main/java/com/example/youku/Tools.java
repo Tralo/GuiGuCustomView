8229c69e22253db7c3293e6f09c6c004d39d3558
Tralo <13580548169@163.com>
2016-09-08 05:45:23 +0800

    设置子View不可点击完成隐藏View不可点击功能

--- youku/src/main/java/com/example/youku/Tools.java
+++ youku/src/main/java/com/example/youku/Tools.java
@@ -1,6 +1,7 @@
 package com.example.youku;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.RotateAnimation;
 import android.widget.RelativeLayout;
 
@@ -8,27 +9,35 @@ import android.widget.RelativeLayout;
  * Created by adventurer on 16-9-8.
  */
 public class Tools {
-    public static void hideView(View view) {
+    public static void hideView(ViewGroup view) {
         hideView(view,0);
     }
 
-    public static void showView(View view) {
+    public static void showView(ViewGroup view) {
         showView(view,0);
     }
 
-    public static void hideView(View view, int startOffset) {
+    public static void hideView(ViewGroup view, int startOffset) {
         RotateAnimation ra = new RotateAnimation(0,180,view.getWidth() / 2, view.getHeight());
         ra.setDuration(500);
         ra.setFillAfter(true);
         ra.setStartOffset(startOffset);//延迟多久后在
         view.startAnimation(ra);
+        for(int i = 0; i < view.getChildCount(); i++){
+            View childView = view.getChildAt(i);
+            childView.setEnabled(false);
+        }
     }
 
-    public static void showView(View view, int startOffset) {
+    public static void showView(ViewGroup view, int startOffset) {
         RotateAnimation ra = new RotateAnimation(180,360,view.getWidth() / 2, view.getHeight());
         ra.setDuration(500);
         ra.setFillAfter(true);
         ra.setStartOffset(startOffset);//延迟多久后在
         view.startAnimation(ra);
+        for(int i = 0; i < view.getChildCount(); i++){
+            View childView = view.getChildAt(i);
+            childView.setEnabled(true);
+        }
     }
 }
