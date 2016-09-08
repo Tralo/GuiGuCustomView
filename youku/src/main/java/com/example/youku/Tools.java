97807d2259d2dac644b74554a49dcc7ab32a272c
Tralo <13580548169@163.com>
2016-09-08 23:49:23 +0800

    属性动画解决点击问题

--- youku/src/main/java/com/example/youku/Tools.java
+++ youku/src/main/java/com/example/youku/Tools.java
@@ -1,9 +1,9 @@
 package com.example.youku;
 
+import android.animation.ObjectAnimator;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.RotateAnimation;
-import android.widget.RelativeLayout;
 
 /**
  * Created by adventurer on 16-9-8.
@@ -18,7 +18,8 @@ public class Tools {
     }
 
     public static void hideView(ViewGroup view, int startOffset) {
-        RotateAnimation ra = new RotateAnimation(0,180,view.getWidth() / 2, view.getHeight());
+        //视图动画
+        /*RotateAnimation ra = new RotateAnimation(0,180,view.getWidth() / 2, view.getHeight());
         ra.setDuration(500);
         ra.setFillAfter(true);
         ra.setStartOffset(startOffset);//延迟多久后在
@@ -26,11 +27,19 @@ public class Tools {
         for(int i = 0; i < view.getChildCount(); i++){
             View childView = view.getChildAt(i);
             childView.setEnabled(false);
-        }
+        }*/
+
+        //属性动画
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view,"rotation",0,180);
+        animator.setDuration(500);
+        animator.setStartDelay(startOffset);
+        animator.start();
+        view.setPivotX(view.getWidth() / 2);
+        view.setPivotY(view.getHeight());
     }
 
     public static void showView(ViewGroup view, int startOffset) {
-        RotateAnimation ra = new RotateAnimation(180,360,view.getWidth() / 2, view.getHeight());
+        /*RotateAnimation ra = new RotateAnimation(180,360,view.getWidth() / 2, view.getHeight());
         ra.setDuration(500);
         ra.setFillAfter(true);
         ra.setStartOffset(startOffset);//延迟多久后在
@@ -38,6 +47,12 @@ public class Tools {
         for(int i = 0; i < view.getChildCount(); i++){
             View childView = view.getChildAt(i);
             childView.setEnabled(true);
-        }
+        }*/
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view,"rotation",180,360);
+        animator.setDuration(500);
+        animator.setStartDelay(startOffset);
+        animator.start();
+        view.setPivotX(view.getWidth() / 2);
+        view.setPivotY(view.getHeight());
     }
 }
