90de996a3271b380712d76b5a6020c1ebc6661ff
Tralo <13580548169@163.com>
2016-09-10 16:39:01 +0800

    点击事件添加

--- adbanner/src/main/java/com/example/adbanner/MainActivity.java
+++ adbanner/src/main/java/com/example/adbanner/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.adbanner;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         "钢铁侠","美国队长","东京史诗贵","复仇者联盟","鼠会"
     };
 
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            int item = vp_banner.getCurrentItem() + 1;
+            vp_banner.setCurrentItem(item);
+
+            mHandler.sendEmptyMessageDelayed(0,4000);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         //设置中间位置
         int item = Integer.MAX_VALUE/2 - Integer.MAX_VALUE/2%imageViews.size();//要保证imageViews的整数倍
         vp_banner.setCurrentItem(item);
+
+        mHandler.sendEmptyMessageDelayed(0,4000);
     }
 
     private void initAdapter() {
@@ -65,14 +81,28 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageScrollStateChanged(int state) {
+                if(state == ViewPager.SCROLL_STATE_DRAGGING){
+                    isDragging = true;
+
+                } else if(state == ViewPager.SCROLL_STATE_SETTLING){
+
+                } else if(state == ViewPager.SCROLL_STATE_IDLE && isDragging){
+                    mHandler.removeCallbacksAndMessages(null);
+                    mHandler.sendEmptyMessageDelayed(0,4000);
+                }
 
             }
         });
     }
 
+    /**
+     * 是否滚动
+     */
+    private boolean isDragging = false;
+
     private void initChildPage() {
         for(int i = 0; i < imageIds.length; i++){
-            ImageView iv = new ImageView(this);
+            final ImageView iv = new ImageView(this);
             iv.setBackgroundResource(imageIds[i]);
 
             imageViews.add(iv);
@@ -90,6 +120,35 @@ public class MainActivity extends AppCompatActivity {
 
 
             ll_point_group.addView(iv_point);
+
+            iv.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    switch (motionEvent.getAction()){
+                        case MotionEvent.ACTION_DOWN:
+                            mHandler.removeCallbacksAndMessages(null);
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            break;
+                        case MotionEvent.ACTION_CANCEL:
+                            mHandler.removeCallbacksAndMessages(null);
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            mHandler.sendEmptyMessageDelayed(0,4000);
+                            break;
+                    }
+
+                    return false;//不要消费掉，不然没法触发点击事件
+                }
+            });
+            iv.setTag(i);
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int index = (int) iv.getTag();
+                    Toast.makeText(MainActivity.this,"Postion = "+ index +"Text? " + titles[index],Toast.LENGTH_SHORT).show();
+                }
+            });
 
         }
 
