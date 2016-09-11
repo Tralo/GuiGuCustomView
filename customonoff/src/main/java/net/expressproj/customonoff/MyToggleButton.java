1335b0f09f1efd42cdee4f850d90a9c4ec1e13f9
Tralo <13580548169@163.com>
2016-09-11 21:05:29 +0800

    滑动开关实现

--- customonoff/src/main/java/net/expressproj/customonoff/MyToggleButton.java
+++ customonoff/src/main/java/net/expressproj/customonoff/MyToggleButton.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
@@ -17,8 +18,10 @@ public class MyToggleButton extends View implements View.OnClickListener{
     private Bitmap backGroundBItmap;
     private Bitmap slidingBitmap;
     private int slidLeftMax;
+    private float slidLeft;
     private Paint paint;
     private boolean isOpen = false;
+    private boolean isEnableClick = true;
 
     /**
      * 如果我们在布局文件使用该类，将会用这个构造方法使用该类，如果没有就会崩溃
@@ -54,18 +57,79 @@ public class MyToggleButton extends View implements View.OnClickListener{
     protected void onDraw(Canvas canvas) {
 //        super.onDraw(canvas);
         canvas.drawBitmap(backGroundBItmap,0,0,paint);
-        if(isOpen){
-            canvas.drawBitmap(slidingBitmap,slidLeftMax,0,paint);
-        } else {
-            canvas.drawBitmap(slidingBitmap,0,0,paint);
-        }
+        canvas.drawBitmap(slidingBitmap,slidLeft,0,paint);
     }
 
 
     @Override
     public void onClick(View view) {
         Toast.makeText(getContext(),"被点击了",Toast.LENGTH_SHORT).show();
-        isOpen = !isOpen;
+        if(isEnableClick){
+            isOpen = !isOpen;
+            flushView();
+        }
+
+    }
+
+    private float startX = 0;
+    private float lastX = 0;
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                lastX = event.getX();
+                startX = event.getX();
+                isEnableClick = true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                isEnableClick = false;
+                float endX = event.getX();
+                float distance = endX - startX;
+                slidLeft += distance;
+                if(slidLeft < 0){
+                    slidLeft = 0;
+                }
+                if(slidLeft > slidLeftMax){
+                    slidLeft = slidLeftMax;
+                }
+                startX = event.getX();
+
+                invalidate();
+                if(Math.abs(endX - lastX) > 5){
+                    isEnableClick = false;
+
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+               if(!isEnableClick){
+                   if(slidLeft > slidLeftMax / 2){
+                       isOpen = true;
+
+                   } else {
+
+                       isOpen = false;
+                   }
+                   isEnableClick = false;
+                   flushView();
+               }
+
+
+                break;
+
+        }
+
+        return true;
+    }
+
+    private void flushView() {
+        if(isOpen){
+            slidLeft = slidLeftMax;
+        } else {
+            slidLeft = 0;
+        }
         invalidate();
     }
 }
