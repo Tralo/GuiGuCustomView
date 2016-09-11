e71ee0f966168a0ca08052b1acb3149684e9b4eb
Tralo <13580548169@163.com>
2016-09-11 18:03:05 +0800

    控件绘制到创建的流程

--- customonoff/src/main/java/net/expressproj/customonoff/MainActivity.java
+++ customonoff/src/main/java/net/expressproj/customonoff/MainActivity.java
@@ -3,6 +3,16 @@ package net.expressproj.customonoff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *  一个视图从创建到显示过程中的主要方法
+ *  1.构造方法实例化类
+ *  2.测量measure(int,int) --> onMeasure();
+ *  如果当前View是一个ViewGroup,还有义务测量孩子
+ *  3.测量位置-layout()-->onLayout();
+ *  指定控件的位置,一般View 不用写这个方法，ViewGroup的时候才需要，一般View不需要重写该方法
+ *  4.绘制视图--draw()-->onDraw(canvas)
+ *  根据上面两个方法参数，进入绘制
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ customonoff/src/main/java/net/expressproj/customonoff/MyToggleButton.java
@@ -0,0 +1,19 @@
+package net.expressproj.customonoff;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by adventurer on 16-9-10.
+ */
+public class MyToggleButton extends View {
+    /**
+     * 如果我们在布局文件使用该类，将会用这个构造方法使用该类，如果没有就会崩溃
+     * @param context
+     * @param attrs
+     */
+    public MyToggleButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
