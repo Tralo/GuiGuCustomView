01a2b35bd6ff01206f78aae041bb4bccb03cad38
Tralo <13580548169@163.com>
2016-09-13 08:43:50 +0800

    update

--- followviewpager/src/main/java/net/expressproj/followviewpager/MainActivity.java
+++ followviewpager/src/main/java/net/expressproj/followviewpager/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyViewPager mvp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mvp = (MyViewPager) findViewById(R.id.mvp);
     }
 }
---
+++ followviewpager/src/main/java/net/expressproj/followviewpager/MyViewPager.java
@@ -0,0 +1,21 @@
+package net.expressproj.followviewpager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by adventurer on 16-9-13.
+ */
+public class MyViewPager extends ViewGroup {
+
+
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
